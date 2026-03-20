faae513c3ab9f9358ff0b2b1c508aff01c4f5fce
João Camargo <j.camargo@edu.unifil.br>
2026-03-20 17:27:24 -0300

    api funcionando, criei get e post livros, pronto para testar no postman

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 // 1. Importar Express
 const express = require('express');
 
@@ -9,6 +11,67 @@ const PORT = 3000;
 
 // 4. Middleware para JSON
 app.use(express.json());
+
+let livros = [];
+let id = 1;
+app.get('/livros', (req, res) => {
+  res.json(livros);
+});
+app.post('/livros', (req, res) => {
+  const { titulo, autor, ano } = req.body;
+
+  // validações
+  if (!titulo) {
+    return res.status(400).json({ erro: "Título é um campo obrigatório." });
+  }
+
+  if (!autor) {
+    return res.status(400).json({ erro: "Autor é um campo obrigatório." });
+  }
+
+  if (typeof ano !== 'number') {
+    return res.status(400).json({ erro: "Ano deve ser digitado com números inteiros" });
+  }
+
+  const novoLivro = {
+    id: id++,
+    titulo,
+    autor,
+    ano
+  };
+
+  livros.push(novoLivro);
+
+  res.status(201).json(novoLivro);
+});
+
+app.post('/livros', (req, res) => {
+  const { titulo, autor, ano } = req.body;
+
+  // validações
+  if (!titulo) {
+    return res.status(400).json({ erro: "Título é um campo obrigatório" });
+  }
+
+  if (!autor) {
+    return res.status(400).json({ erro: "Autor é um campo obrigatório" });
+  }
+
+  if (typeof ano !== 'number') {
+    return res.status(400).json({ erro: "Ano deve ser um número inteiro!" });
+  }
+
+  const novoLivro = {
+    id: id++,
+    titulo,
+    autor,
+    ano
+  };
+
+  livros.push(novoLivro);
+
+  res.status(201).json(novoLivro);
+});
 
 // 5. Criar primeiro endpoint
 app.get('/', (req, res) => {
